9e281b0ba8174d348b54c1f5d1ddf5ff0cd554ab
darshit001 <darshitradadiya38@gmail.com>
2023-12-30 16:56:52 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import reportWebVitals from './reportWebVitals'
+import './index.css'
+import App from './App'
+// import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+// reportWebVitals()
+// serviceWorker.register()
